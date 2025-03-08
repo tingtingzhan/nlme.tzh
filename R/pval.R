932bfa6752bb003bde8defd781d2b1172fc18d2f
Tingting Zhan <tingtingzhan@gmail.com>
2025-03-08 13:10:28 -0500

    test

--- R/pval.R
+++ R/pval.R
@@ -4,35 +4,14 @@
 #' 
 #' @param x \link[nlme]{lme} or \link[nlme]{gls} object
 #' 
-#' @examples
-#' .pval.lme(ortho1)
-#' .pval.gls(ovary1)
-#' 
 #' @name pval_nlme
-#' @export
-.pval.gls <- function(x) {
-  x |>
-    summary() |> # ?nlme:::summary.gls
-    .pval.summary.gls()
-}
-
-
-#' @rdname pval_nlme
-#' @export
-.pval.lme <- function(x) {
-  x |> 
-    summary() |> # ?nlme:::summary.lme
-    .pval.summary.lme()
-}
-
-
-#' @rdname pval_nlme
 #' @export
 .pval.summary.lme <- function(x) {
   # `nlme:::coef.summary.lme` not show-able!!  could only see from debug(coef)
   # `nlme:::coef.summary.gls` not show-able!!  could only see from debug(coef)
-  ret <- x$tTable[, 'p-value'] 
-  names(ret) <- rownames(x$tTable)
+  cf <- x$tTable
+  ret <- cf[, 'p-value'] 
+  names(ret) <- rownames(cf)
   return(ret)
 }
 
