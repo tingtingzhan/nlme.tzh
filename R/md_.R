a1761592623b334ef6f3a9ebce3c5f94e3deb245
Tingting Zhan <tingtingzhan@gmail.com>
2026-02-09 23:16:00 -0500

    tzh

--- R/md_.R
+++ R/md_.R
@@ -11,17 +11,17 @@
 #'   data = Ovary, correlation = corAR1(form = ~ 1 | Mare)) 
 #'   
 #' library(ecip)
-#' list('`lme`' = m1, '`gls`' = m2) |> rmd.tzh::render_(file = 'lme_gls')
+#' list('`lme`' = m1, '`gls`' = m2) |> fastmd::render_(file = 'lme_gls')
 #' @keywords internal
 #' @name md_nlme
-#' @importFrom rmd.tzh md_
+#' @importFrom fastmd md_
 #' @importFrom ecip md_ecip
 #' @export md_.lme
 #' @export
 md_.lme <- md_ecip
 
 #' @rdname md_nlme
-#' @importFrom rmd.tzh md_
+#' @importFrom fastmd md_
 #' @importFrom ecip md_ecip
 #' @export md_.gls
 #' @export
